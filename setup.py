ad9043494f44ef62a6b87d358f2aed2cd09efacf
Maxime Sahuc <maxime.sahuc76@gmail.com>
2024-11-28 18:32:57 +0100

    fix requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
 setup(name='deezer-dl',
       version='0.7',
       description='Deezer music downloader',
@@ -9,6 +13,7 @@ setup(name='deezer-dl',
       author_email='',
       packages=['deezerdl', 'deezer'],
       package_dir={'deezerdl': 'deezerdl', 'deezer': 'deezerdl/deezer'},
+      install_requires=requirements,
       entry_points = {
             'console_scripts': ['deezer-dl=deezerdl.main:main'],
       },
